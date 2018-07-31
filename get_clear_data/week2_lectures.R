1576b75910c03b08cc03ce691396da5bf28f149c
Kirill Setdekov <ksetdekov@gmail.com>
2018-07-31 15:55:03 +0300

    week 2 complete

--- get_clear_data/week2_lectures.R
+++ get_clear_data/week2_lectures.R
@@ -25,7 +25,7 @@ affyData<-dbReadTable(hg19,"affyU133Plus2")
 head(affyData)
 
 #selecting only a subset of the table
-query<-sbSendQuety(hg19,"select * from affyU133Plus2 where  misMatches between 1 and 3")
+query<-dbSendQuery(hg19,"select * from affyU133Plus2 where  misMatches between 1 and 3")
 affyMis<-fetch(query);quantile(affyMis$Matches)
 #send a querry
 affyMisSmall<-fetch(query,n=10);dbClearResult(query); 
@@ -40,4 +40,127 @@ dbDisconnect(hd19)
 
 #### reading from HDF5
 ## hierarchichal data type
+source("http://bioconductor.org/biocLite.R")
+biocLite("rhdf5")
 
+library(rhdf5)
+create = h5createFile("example.H5")
+create
+
+##creating groups
+create=h5createGroup("example.H5","foo")
+create=h5createGroup("example.H5","baa")
+create=h5createGroup("example.H5","foo/foobaa") ## subgroups
+
+##loop at files
+h5ls("example.H5")
+
+# write to groups
+A=matrix(1:10,nr=5,nc=2)
+h5write(A,"example.H5","foo/a") ## write to a group
+B =array(seq(0.1,2.0,by=0.1),dim = c(5,2,2))
+attr(B,"scale") <- "liter"
+h5write(B,"example.H5","foo/foobaa/B") ## write to a group
+h5ls("example.H5")
+
+#write a data set directly into root of HDF5
+df=data.frame(1L:5L, seq(0,1, length.out=5),c("ab","cde","fghi","a","s"), stringsAsFactors=FALSE)
+h5write(df,"example.H5","df")
+h5ls("example.H5")
+
+## reading
+readA=h5read("example.H5","foo/a")
+readB=h5read("example.H5","foo/foobaa/B")
+readdf=h5read("example.H5","df")
+readA
+readdf
+H5close()
+## writing into a part of the file
+h5write(c(12,13,14),"example.H5","foo/a", index=list(1:3,1))
+h5read("example.H5","foo/a")
+
+
+## reading data from the web
+con = url("http://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en")
+htmlCode = readLines(con)
+close(con)
+head(htmlCode,1)
+
+## better - parse with xml
+library(XML)
+url <- "https://scholar.google.com/citations?user=HI-I6C0AAAAJ&hl=en"
+html <- htmlTreeParse(url, useInternalNodes=T)
+
+xpathSApply(html, "//title", xmlValue)
+xpathSApply(html, "//td[@id='col-citedby']", xmlValue)
+
+## using httr pachage
+library(httr); html2 = GET(url)
+content2 = content(html2,as="text")
+parsedHtml = htmlParse(content2,asText=TRUE)
+xpathSApply(parsedHtml, "//title", xmlValue)
+
+
+#using passwords
+pg1 = GET("http://httpbin.org/basic-auth/user/passwd")
+pg1
+
+## accessing
+pg2 = GET("http://httpbin.org/basic-auth/user/passwd",
+          authenticate("user","passwd"))
+pg2
+names(pg2)
+
+## handles
+google = handle("http://google.com")
+pg1 = GET(handle=google,path="/")
+pg2 = GET(handle=google,path="search")
+
+## reading from API's
+##Accessing Twitter from R
+myapp = oauth_app("twitter",
+                  key="yourConsumerKeyHere",secret="yourConsumerSecretHere")
+sig = sign_oauth1.0(myapp,
+                    token = "yourTokenHere",
+                    token_secret = "yourTokenSecretHere")
+homeTL = GET("https://api.twitter.com/1.1/statuses/home_timeline.json", sig)
+
+##Converting the json object
+json1 = content(homeTL)
+json2 = jsonlite::fromJSON(toJSON(json1))
+json2[1,1:4]
+
+##httr allows GET, POST, PUT, DELETE requests if you are authorized
+
+## quiz
+csvlint<-"https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+acs<-read.csv(csvlint)
+install.packages("sqldf" )
+library("sqldf")
+sqldf("select pwgtp1 from acs where AGEP < 50")
+
+##testing distinct
+uniqueacs<-unique(acs$AGEP)
+
+uniquesql<-sqldf("select distinct AGEP from acs")
+
+##
+#How many characters are in the 10th, 20th, 30th and 100th lines of HTML from this page:
+        
+#        http://biostat.jhsph.edu/~jleek/contact.html
+
+#(Hint: the nchar() function in R may be helpful)
+
+con = url("http://biostat.jhsph.edu/~jleek/contact.html")
+htmlCode = readLines(con)
+close(con)
+otputvector<-c(1:4)
+for (i in c(10,20,30,100)) {
+        otputvector[i]<-nchar(htmlCode[i])
+}
+otputvector[c(10,20,30,100)]
+## read fwf
+fwfurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fwksst8110.for"
+
+forsum<-read.fwf(fwfurl, widths = c(10,9,4,9),skip = 4)
+sum(forsum$V4)
