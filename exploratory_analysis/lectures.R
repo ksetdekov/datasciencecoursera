6c7147085c47b1ceec118e2e5be7374f26c5d6fe
Kirill Setdekov <ksetdekov@gmail.com>
2019-01-04 17:54:18 +0300

    week 4 added with ctree hacking

--- exploratory_analysis/lectures.R
+++ exploratory_analysis/lectures.R
@@ -1,3 +1,5 @@
+setwd("exploratory_analysis")
+
 #week 1 ########################################################################
 library(datasets)
 data("cars")
@@ -656,3 +658,73 @@ smoothScatter(x,y) # 2d hist
 #using transparency
 plot(x,y,pch=19)
 plot(x,y,col=rgb(0,0,0,0.01), pch=19)
+
+#week 4 #####
+load("./samsungData.rda")
+names(samsungData)[1:12]
+table(samsungData$activity)
+samsungData$activity2 <- factor(samsungData$activity)
+cfit1 <- ctree(samsungData$activity2 ~ ., data = samsungData[,1:400])
+#plot(cfit1)
+samsungData$activity2pred <- predict(cfit1, samsungData[,1:400])
+library(InformationValue)
+library(MLmetrics)
+Accuracy(samsungData$activity2pred,samsungData$activity)
+ConfusionMatrix(samsungData$activity2pred,samsungData$activity)
+png("tree.png", res=80, height=2000, width=40000) 
+plot(cfit1) 
+dev.off()
+
+#plot acitvities
+#best feature
+par(mfrow=c(1,2))
+numericActivity<-as.numeric(as.factor(samsungData$activity))[samsungData$subject==30]
+plot(samsungData[samsungData$subject==30,367],pch=19,col=numericActivity,ylab=names(samsungData)[367])
+plot(samsungData[samsungData$subject==30,368],pch=19,col=numericActivity,ylab=names(samsungData)[368])
+legend(150,-0.1,legend=unique(samsungData$activity),col=unique(numericActivity),pch=19)
+
+distanceMatrix<-dist(samsungData[samsungData$subject==1,367:369])
+hclustering<-hclust(distanceMatrix)
+myplclust(hclustering,lab.col=numericActivity)
+
+#clustering
+source("prettyclust.R")
+distanceMatrix<-dist(samsungData[samsungData$subject==1,1:3])
+hclustering<-hclust(distanceMatrix)
+myplclust(hclustering,lab.col=numericActivity)
+
+#max acceleration
+par(mfrow=c(1,2))
+plot(samsungData[samsungData$subject==1,10],pch=19,col=numericActivity,ylab=names(samsungData)[10])
+plot(samsungData[samsungData$subject==1,11],pch=19,col=numericActivity,ylab=names(samsungData)[11])
+
+distanceMatrix<-dist(samsungData[samsungData$subject==1,10:12])
+hclustering<-hclust(distanceMatrix)
+myplclust(hclustering,lab.col=numericActivity)
+#Singular value decomposition
+svd1=svd(scale(samsungData[samsungData$subject==1,-c(562:565)]))
+par(mfrow=c(1,2))
+plot(svd1$u[,1],col=numericActivity,pch=19) # singular vector
+plot(svd1$u[,2],col=numericActivity,pch=19)
+
+dev.off()
+##Find maximum contributor
+plot(svd1$v[,2],pch=19)
+
+maxContrib<-which.max(svd1$v[,2])
+distanceMatrix<-dist(samsungData[samsungData$subject==1,c(10:12,maxContrib)])
+hclustering<-hclust(distanceMatrix)
+myplclust(hclustering,lab.col=numericActivity)
+names(samsungData)[maxContrib]
+
+#K-means clustering (nstart=1, first try)
+
+kClust<-kmeans(samsungData[samsungData$subject==1,-c(562:565)],centers=6)
+table(kClust$cluster,samsungData$activity[samsungData$subject==1])
+
+#second try
+kClust<-kmeans(samsungData[samsungData$subject==1,-c(562:565)],centers=6, nstart = 100)
+table(kClust$cluster,samsungData$activity[samsungData$subject==1])
+
+plot(kClust$center[1,367:377],pch=19,ylab="ClusterCenter",xlab="")
+plot(kClust$center[6,367:377],pch=19,ylab="ClusterCenter",xlab="")
