4e17096d65713e9f5bb8427289813e06237270e5
Kirill Setdekov <ksetdekov@gmail.com>
2019-01-05 13:27:52 +0300

    finished lectures week4

--- exploratory_analysis/lectures.R
+++ exploratory_analysis/lectures.R
@@ -772,4 +772,58 @@ site0 <- as.data.frame(site0)
 site1 <- as.data.frame(site1)
 site0 <- paste(site0[,1],site0[,2], sep = ".")
 site1 <- paste(site1[,1],site1[,2], sep = ".")
-#finish at min 22
+
+both <- intersect(site0, site1)
+
+#selecting both
+pm0$conty.site <- with(pm0, paste(County.Code, Site.ID, sep = "."))
+pm1$conty.site <- with(pm1, paste(County.Code, Site.ID, sep = "."))
+#select only ones from NY and this both info
+cnt0 <- subset(pm0, State.Code==36 & conty.site %in% both)
+cnt1 <- subset(pm1, State.Code==36 & conty.site %in% both)
+
+sapply(split(cnt0, cnt0$conty.site),nrow) 
+sapply(split(cnt1, cnt1$conty.site),nrow) 
+
+pm1sub <- subset(pm1, State.Code==36&County.Code==1& Site.ID==5)
+pm0sub <- subset(pm0, State.Code==36&County.Code==1& Site.ID==5)
+
+dim(pm1sub)
+dim(pm0sub)
+dates1 <- pm1sub$Date
+dates1 <- as.Date(as.character(dates1), "%Y%m%d")
+x1sub <- pm1sub$Sample.Value
+x0sub <- pm0sub$Sample.Value
+
+plot(dates1,x1sub)
+
+dates0 <- pm0sub$Date
+dates0 <- as.Date(as.character(dates0), "%Y%m%d")
+plot(dates0,x0sub)
+
+rng <- range(x0sub,x0sub, na.rm = T)
+par(mfrow=c(1,2))
+plot(dates0, x0sub, pch=20, ylim = rng)
+abline(h=median(x0sub,na.rm = T))
+plot(dates1, x1sub, pch=20, ylim = rng)
+abline(h=median(x1sub,na.rm = T))
+dev.off()
+
+#tabpply
+mn0 <- with(pm0, tapply(Sample.Value, State.Code, mean,na.rm=T))
+summary(mn0)
+mn1 <- with(pm1, tapply(Sample.Value, State.Code, mean,na.rm=T))
+summary(mn1)
+
+d0 <- data.frame(state=names(mn0), mean=mn0)
+d1 <- data.frame(state=names(mn1), mean=mn1)
+
+mrg <- merge(d0,d1, by="state")
+library(ggplot2)
+qplot(mean.x, mean.y, data=mrg)+geom_abline()+geom_smooth(method = "lm")
+
+#segments going down
+par(mfrow=c(1,1))
+with(mrg, plot(rep(1999,52), mrg[,2], xlim=c(1998, 2013)))
+with(mrg, points(rep(2012,52), mrg[,3]))
+segments(rep(1999,52),mrg[,2], rep(2012,52), mrg[,3])
