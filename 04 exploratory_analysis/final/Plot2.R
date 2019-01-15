3adf6564010dcd9099999fbcca56707ab97657b7
Kirill Setdekov <ksetdekov@gmail.com>
2019-01-15 16:15:29 +0300

    plot2

--- 04 exploratory_analysis/final/Plot2.R
+++ 04 exploratory_analysis/final/Plot2.R
@@ -10,3 +10,20 @@ SCC <-
 ##2Have total emissions from PM2.5 decreased in the Baltimore City, Maryland
 ##(\color{red}{\verb|fips == "24510"|}fips=="24510") from 1999 to 2008? Use the
 ##base plotting system to make a plot answering this question.
+
+library(dplyr)
+yearly <-
+        NEI %>%  filter(fips == "24510") %>% group_by(year) %>%     summarise(Emissions = sum(Emissions))
+png(
+        file = "plot2.png",
+        width = 480,
+        height = 480,
+        bg = "white"
+)
+with(yearly, plot(
+        year,
+        Emissions,
+        type = "l",
+        xaxp  = c(1999, 2008, 9)
+))
+dev.off()
