b2e69761ee83592e2c9461c4799d33439daa1219
Kirill Setdekov <ksetdekov@gmail.com>
2019-01-17 13:25:46 +0300

    finished plots 5 and 6

--- 04 exploratory_analysis/final/Plot5.R
+++ 04 exploratory_analysis/final/Plot5.R
@@ -9,3 +9,35 @@ SCC <-
 
 ##5How have emissions from motor vehicle sources changed from 1999–2008 in
 ##Baltimore City?
+library(stringr)
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+SCC_motor <-
+        SCC[str_detect(SCC$EI.Sector, regex("vehic", ignore_case = TRUE)), ]
+vehid <- lapply(SCC_motor$SCC, as.character)
+NEIveh <- NEI[NEI$SCC %in% vehid, ]
+
+yearly <-
+        NEI %>%  filter(fips == "24510") %>% group_by(year) %>%     summarise(Emissions = sum(Emissions))
+names(yearly)[2] <- "baltimore"
+yearlyall <-
+        NEI %>% group_by(year) %>%     summarise(Emissions = sum(Emissions))
+names(yearlyall)[2] <- "all"
+total <- merge(yearly,yearlyall,by="year")
+total$baltimore <- log10(total$baltimore )
+total$all <- log10(total$all )
+
+total_long <- melt(total, id="year")  # convert to long format
+
+
+g <- ggplot(total_long, aes(x = year, y = value, colour=variable))+geom_point()
+png(
+        file = "plot5.png",
+        width = 480,
+        height = 480,
+        bg = "white"
+)
+g +geom_smooth(aes(x = year, y = value), method = "lm", se = FALSE, col = "steelblue") +theme_bw()+facet_grid(.~variable)+
+        labs(y = "log 10 PM2.5 Emissions in tons/year") + labs(title = "Pollution dynamics from Motor vehicles for whole US vs Balitmore")
+dev.off()
--- 04 exploratory_analysis/final/Plot6.R
+++ 04 exploratory_analysis/final/Plot6.R
@@ -11,3 +11,35 @@ SCC <-
 ##from motor vehicle sources in Los Angeles County, California
 ##(\color{red}{\verb|fips == "06037"|}fips=="06037"). Which city has seen
 ##greater changes over time in motor vehicle emissions?
+library(stringr)
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+SCC_motor <-
+        SCC[str_detect(SCC$EI.Sector, regex("vehic", ignore_case = TRUE)), ]
+vehid <- lapply(SCC_motor$SCC, as.character)
+NEIveh <- NEI[NEI$SCC %in% vehid, ]
+
+yearly <-
+        NEI %>%  filter(fips == "24510") %>% group_by(year) %>% summarise(Emissions = sum(Emissions))
+names(yearly)[2] <- "Baltimore"
+yearlyLA <-
+        NEI %>% filter(fips == "06037") %>%  group_by(year) %>% summarise(Emissions = sum(Emissions))
+names(yearlyLA)[2] <- "Los_Angeles"
+total <- merge(yearly, yearlyLA, by = "year")
+total$Baltimore <- (total$Baltimore) / total$Baltimore[1]
+total$Los_Angeles <- (total$Los_Angeles) / total$Los_Angeles[1]
+
+total_long <- melt(total, id = "year")  # convert to long format
+
+
+g <-
+        ggplot(total_long, aes(x = year, y = value, colour = variable)) + geom_line()
+png(
+        file = "plot6.png",
+        width = 480,
+        height = 480,
+        bg = "white"
+)
+g + theme_bw() + labs(y = "relative change of PM 2.5 (1999 as base)") + labs(title = "Relative pollution dynamics from Motor vehicles for Balitmore vs Los Angeles")
+dev.off()
