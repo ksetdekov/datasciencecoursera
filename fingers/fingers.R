daca33f9cbb56b0945b6e2c391196667443f199a
ksetdekov <ksetdekov@gmail.com>
2019-03-30 19:42:37 +0300

    fingers

--- fingers/fingers.R
+++ fingers/fingers.R
@@ -1,11 +1,42 @@
-library(googlesheets)
-gs_ls()
-head(gs_ls())
-gsheets <- gs_ls()
-gsheets$sheet_title
-gsheets$sheet_title[1]
-gs_title("Про пальцы (Ответы)")
-fingers <- gs_title("Про пальцы (Ответы)")
-gs_ws_ls(fingers)
-resp <- gs_read(ss=fingers, ws = 1, skip=0)
-View(resp)
+library(googledrive)
+drive_find(n_max = 50)
+as_id("1A09wH_fi6NcGAQQU3Q3vihW2ZS5kxJhjET0cdmEe4xY")
+remote <- drive_get(as_id("1A09wH_fi6NcGAQQU3Q3vihW2ZS5kxJhjET0cdmEe4xY"))
+drive_download(remote, path = "fingers", type = "csv")
+
+library(readr)
+fingers <- read_csv("fingers.csv", col_types = cols(`Отметка времени` = col_datetime(format = "%d.%m.%Y %H:%M:%S")))
+View(fingers)
+
+require(dplyr)
+require(stringi)
+require(lubridate)
+
+names(fingers) <- tolower(stri_trans_general(names(fingers), "russian-latin/bgn"))
+
+names(fingers) <- c("time","damage","activity") 
+
+fingers_mod <-
+    fingers %>% 
+    mutate(damage = tolower(stri_trans_general(damage, "russian-latin/bgn"))) %>% 
+    mutate(damage = ifelse(damage == "da", 1, 0)) %>% 
+    mutate(damage = factor(damage,
+                           levels = c(1, 0),
+                           labels = c("yes", "no")
+            )) %>% 
+    mutate(daystart=floor_date(time, "day")) %>% 
+    mutate(dayhour=as.numeric(time-daystart)/3600)
+
+fingers_mod <- fingers_mod %>% 
+    mutate(day=    as.numeric((daystart-startingday)/86400)) %>% 
+    mutate(weekday =as.numeric(factor(weekdays(time))))
+
+startingday <- min(fingers_mod$daystart)
+require(party)
+cfit <- ctree(damage~dayhour, data = fingers_mod)
+plot(cfit)
+
+cfit2 <- mob(damage~dayhour|dayhour, data = fingers_mod,model = glinearModel, 
+             family = binomial())
+plot(cfit2)
+
