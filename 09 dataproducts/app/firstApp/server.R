76765f01c58e439e3128b06160580df1878a4f37
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-10 13:09:06 +0300

    additng a random - it only randomizes 1 time when reading input.

--- 09 dataproducts/app/firstApp/server.R
+++ 09 dataproducts/app/firstApp/server.R
@@ -1,6 +1,6 @@
 library(shiny)
 shinyServer(
     function(input, output) {
-        output$text1 = renderText(input$slider2)
+        output$text1 = renderText(input$slider2+sample(1:10,1))
     }
 )
