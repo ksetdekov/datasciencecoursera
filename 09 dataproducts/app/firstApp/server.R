0facdb5f392dbf795b37b2282e100d56e6f856df
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-10 15:37:24 +0300

    this plots a rnorm

--- 09 dataproducts/app/firstApp/server.R
+++ 09 dataproducts/app/firstApp/server.R
@@ -1,6 +1,20 @@
 library(shiny)
-shinyServer(
-    function(input, output) {
-        output$text1 = renderText(input$slider2+sample(1:10,1))
-    }
-)
+shinyServer(function(input, output) {
+    output$plot1 <- renderPlot({
+                number_of_points <- input$numeric
+        minX <- input$sliderX[1]
+        maxX <- input$sliderX[2]
+        minY <- input$sliderY[1]
+        maxY <- input$sliderY[2]
+        # dataX <- runif(number_of_points, minX, maxX)
+        dataY <- rnorm(number_of_points, (maxY/2+minY/2), (maxY-(maxY/2+minY/2))/qnorm(input$sliderP, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE))
+        dataX <- rnorm(number_of_points, (maxX/2+minX/2), (maxX-(maxX/2+minX/2))/qnorm(input$sliderP, mean = 0, sd = 1, lower.tail = TRUE, log.p = FALSE))
+        # dataY <- runif(number_of_points, minY, maxY)
+        xlab <- ifelse(input$show_xlab, "X Axis", "")
+        ylab <- ifelse(input$show_ylab, "Y Axis", "")
+        main <- ifelse(input$show_title, "Title", "")
+        plot(dataX, dataY, xlab = xlab, ylab = ylab, main = main,
+             xlim = c(-100, 100), ylim = c(-100, 100))
+            })
+    output$meanX <-  renderText(paste0(input$sliderX[2],input$sliderX[1]))
+})
--- 09 dataproducts/app/firstApp/ui.R
+++ 09 dataproducts/app/firstApp/ui.R
@@ -1,12 +1,24 @@
 library(shiny)
 shinyUI(fluidPage(
-    headerPanel("Data science FTW!"),
-    sidebarPanel(
-        h1("move the slider"),
-        sliderInput("slider2","Slide me",0,100,0)
-    ),
-    mainPanel(
-        h3('slider value:'),
-        textOutput("text1")
+    titlePanel("Plot Random Numbers"),
+    sidebarLayout(
+        sidebarPanel(
+            numericInput("numeric", "How Many Random Numbers Should be Plotted?", 
+                         value = 1000, min = 1, max = 1000, step = 1),
+            sliderInput("sliderX", "Pick Minimum and Maximum X Values",
+                        -100, 100, value = c(-50, 50)),
+            sliderInput("sliderY", "Pick Minimum and Maximum Y Values",
+                        -100, 100, value = c(-50, 50)),
+            sliderInput("sliderP", "probabilit to be in Range",
+                        0.9,0.9999, 100, value = 0.95, step = 0.0001),     
+            checkboxInput("show_xlab", "Show/Hide X Axis Label", value = TRUE),
+            checkboxInput("show_ylab", "Show/Hide Y Axis Label", value = TRUE),
+            checkboxInput("show_title", "Show/Hide Title")
+        ),
+        mainPanel(
+            h3("Graph of Random Points"),
+            plotOutput("plot1"),
+            verbatimTextOutput("meanX")
+        )
     )
 ))
