6fe4c0d0da662c1d17b33c1507cfd6e4eec3690f
Kirill Setdekov <ksetdekov@gmail.com>
2020-02-10 12:55:18 +0300

    different levels of text

--- 09 dataproducts/app/firstApp/ui.R
+++ 09 dataproducts/app/firstApp/ui.R
@@ -2,7 +2,10 @@ library(shiny)
 shinyUI(fluidPage(
     headerPanel("Data science FTW!"),
     sidebarPanel(
-        h3('Sidebar text')
+        h1("h1 tst"),
+        h2("h2"),
+        h3('Sidebar text h3'),
+        em("emphasis test")
     ),
     mainPanel(
         h3('Main Panel text')
